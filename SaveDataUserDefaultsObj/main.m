2cdd9846458bc81a1e6b18d3ddfae1b551a62736
LuyenBG <vanhoangluyen@gmail.com>
2018-01-23 10:31:28 +0700

    Initial Commit

---
+++ SaveDataUserDefaultsObj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SaveDataUserDefaultsObj
+//
+//  Created by LuyenBG on 1/23/18.
+//  Copyright © 2018 LuyenBG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
